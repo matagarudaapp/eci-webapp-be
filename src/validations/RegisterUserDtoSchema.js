4926523ce711f25b7389a40ba74992dca19fdb55
Fairuz <fairuz.satria@ui.ac.id>
2024-06-07 08:44:07 +0700

    fix joi validation

--- src/validations/RegisterUserDtoSchema.js
+++ src/validations/RegisterUserDtoSchema.js
@@ -1,10 +1,10 @@
 const Joi = require('joi');
 
-const userDtoSchema = Joi.object({
+const RegisterUserDtoSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
   role: Joi.string().valid('USER', 'INSTANSI').required()
 });
 
-module.exports = userDtoSchema;
+module.exports = RegisterUserDtoSchema;
