5466088d153c5dea12e34892f14f3f8a56d1f886
Fairuz <fairuz.satria@ui.ac.id>
2024-06-07 08:59:02 +0700

    VideoResult belongs to User

--- src/models/associations.js
+++ src/models/associations.js
@@ -1,5 +1,6 @@
 module.exports.init = (User, Role, VideoResult) => {
     User.belongsToMany(Role, { through: 'UserRoles' });
     Role.belongsToMany(User, { through: 'UserRoles' });
+    VideoResult.belongsTo(User, { foreignKey: 'userId' });
 }
 
