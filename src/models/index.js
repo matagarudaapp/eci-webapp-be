ba22f80b3fef2be255b61c797c8b3743914d9db9
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2024-06-08 19:29:39 +0700

    chore: use env for db connection

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(
-  "postgres://postgres:postgres@localhost:5432/eci-webapp"
-);
+const sequelize = new Sequelize(process.env.DB_URL);
 const User = require("./User")(sequelize, DataTypes);
 const Role = require("./Role")(sequelize, DataTypes);
 const VideoResult = require("./VideoResult")(sequelize, DataTypes);
