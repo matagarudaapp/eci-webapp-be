0435f3a90c15765ab868f1532cd53f300ffa4e5e
matagarudaapp <matagarudaapp@gmail.com>
2024-06-15 21:31:00 +0700

    fix: uncomment uuid joi validation for VideoResult

--- src/validations/InitiateVideoResultSchema.js
+++ src/validations/InitiateVideoResultSchema.js
@@ -4,7 +4,7 @@ const initiateVideoResultSchema = Joi.object({
   videoName: Joi.string().required(),
   inspectionDate: Joi.date().required(),
   inspectorName: Joi.string().required(),
-  // uuid: Joi.string().required(),
+  uuid: Joi.string().required(),
 });
 
 module.exports = initiateVideoResultSchema;
